d8d944241d8fe08f4b391f7ff029788ccbc0c7d6
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-09-10 23:27:34 +0530

    set up view enginer

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,31 @@
 module.exports.home = function (req, res) {
-    return res.end('home from home controller');
+    return res.render('home',{
+        title: "home",
+        data: getTempData(),
+    });
+}
+
+function getTempData() {
+    const val = getData();
+    return [
+        val,
+        val,
+        val,
+        val,
+        val
+    ]
+}
+
+function getData(){
+    return {
+        'id':"asldkfj",
+        'name':"asldkfj",
+        'age':"asldkfj",
+        'gender':"male",
+        'batch':"batch",
+        'status':"status",
+        'dsa_score':"dsa_score",
+        'react_score':"dsa_score",
+        'web_score':"dsa_score",
+    }
 }
--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const router = require('./routes');
 const app = express();
 const port = 8000;
 
-
-
 // use express router
 app.use('/',router);
+
+// setup the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function (err) {
     if(err){
